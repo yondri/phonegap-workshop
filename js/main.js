fb409b2255818d09386269744d9ab2a58fd5c606
Yondri Roa <yondri@gmail.com>
2013-10-16 05:39:50 -0430

    Back to alerts

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
+        var self = this;
+        this.store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(self.employeeLiTpl(employees));
+        });
+        /*console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
             var l = employees.length;
             var e;
@@ -10,12 +14,30 @@ var app = {
                 e = employees[i];
                 $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
             }
-        });
+        });*/
+    },
+
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+
+    renderHomeView: function() {
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        var self = this;
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        
+        this.store = new MemoryStore(function() {
+            self.renderHomeView();
+        });
     }
 
 };
